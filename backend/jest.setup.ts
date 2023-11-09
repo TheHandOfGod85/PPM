348a1ad68d3e421486792f18a188ea2466c491c9
Daniele Del Piano <daniele.delpiano30@gmail.com>
2023-11-09 16:26:46 +0000

    testing

--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -1,1 +1,8 @@
 // jest.setTimeout(5000);
+import { MongoMemoryServer } from 'mongodb-memory-server'
+
+module.exports = async () => {
+  process.env.MONGO_CONNECTION_STRING = (
+    await MongoMemoryServer.create()
+  ).getUri()
+}
