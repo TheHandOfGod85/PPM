dfee496d7130ba050da6d598196d04478a1362a8
Daniele Del Piano <daniele.delpiano30@gmail.com>
2023-11-16 20:31:51 +0000

    refactored

--- backend/src/controllers/part.controller.ts
+++ backend/src/controllers/part.controller.ts
@@ -13,7 +13,6 @@ import 'express-async-errors'
 import mongoose, { Model } from 'mongoose'
 import sharp from 'sharp'
 import env from '../env'
-import { APIfeatures } from '../utils/apiFeatures'
 import { search } from '../utils/search'
 
 export const createPartHandler: RequestHandler<
@@ -70,13 +69,7 @@ export const findPartsHandler: RequestHandler<
   if (!asset) {
     throw createHttpError(404, `No asset found with ID ${req.params.assetId}`)
   }
-
-  const getPartsQuery = await search(
-    PartModel as Model<unknown>,
-    req.query,
-    3,
-    filter
-  )
+  const getPartsQuery = await search(PartModel as Model<unknown>, req.query)
   const parts = await getPartsQuery.result
 
   res.status(200).json({
