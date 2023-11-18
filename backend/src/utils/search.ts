1ce051f9963589014b24509df03184293c6c9e23
Daniele Del Piano <daniele.delpiano30@gmail.com>
2023-11-18 18:24:25 +0000

    implemented search functionality

--- backend/src/utils/search.ts
+++ backend/src/utils/search.ts
@@ -1,4 +1,4 @@
-import { FilterQuery, Model, PipelineStage } from 'mongoose'
+import { FilterQuery, Model } from 'mongoose'
 type QueryObject = Record<string, string>
 export async function search(
   model: Model<unknown>,
@@ -6,38 +6,39 @@ export async function search(
   limit: number = 3,
   filter?: QueryObject
 ) {
-  const paths = Object.keys(model.schema.paths)
-  const filteredPaths = paths.filter((path) => path !== '_id')
   const page = parseInt(query.page || '1')
   const skip = (page - 1) * limit
   let totalItems: number = 0
   let result: FilterQuery<unknown> | undefined
 
-  const pipeline: PipelineStage[] = [
-    { $skip: skip },
-    { $limit: limit },
-    // { $sort: { _id: -1 } },
-  ]
-
-  if (query.search) {
-    const orExpression = filteredPaths.map((path) => ({
-      [path]: { $regex: query.search, $options: 'i' },
-    }))
-    if (filter) {
-      pipeline.unshift({
-        $match: filter,
+  if (query.search && filter) {
+    result = await model
+      .find(filter)
+      .find({
+        $text: {
+          $search: query.search,
+          $caseSensitive: false,
+          $diacriticSensitive: false,
+        },
       })
-    }
-    pipeline.splice(1, 0, {
-      $match: {
-        $or: orExpression,
-      },
-    })
-    result = await model.aggregate(pipeline)
+      .skip(skip)
+      .limit(limit)
     totalItems = result.length
   } else if (filter) {
     result = await model.find(filter).skip(skip).limit(limit).sort('_id')
     totalItems = await model.countDocuments(filter)
+  } else if (query.search) {
+    result = await model
+      .find({
+        $text: {
+          $search: query.search,
+          $caseSensitive: false,
+          $diacriticSensitive: false,
+        },
+      })
+      .skip(skip)
+      .limit(limit)
+    totalItems = await model.countDocuments()
   } else {
     result = await model.find().skip(skip).limit(limit).sort('_id')
     totalItems = await model.countDocuments()
