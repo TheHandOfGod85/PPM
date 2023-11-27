298289b6d8df6e0ea12dbda6416cec8141679564
Daniele Del Piano <daniele.delpiano30@gmail.com>
2023-11-27 18:48:00 +0000

    refactored images patterns

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [{ protocol: 'http', hostname: 'localhost' }],
+    remotePatterns: [
+      { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'https', hostname: 'api.codingart-ddp.com' },
+    ],
     deviceSizes: [640, 768, 1024, 1280],
   },
 }
